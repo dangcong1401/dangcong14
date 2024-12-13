799fd1e0c244982b23b4fa50d68babd132aca642
dangcong14 <dangcong14>
2024-12-13 10:28:46 +0700

    abcabc

---
+++ src/page/clip.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Clip = ({ clip }) => {
+  return (
+    <div className="clip">
+      <h2>{clip.title}</h2>
+      <p>{clip.description}</p>
+      <img src={clip.start} alt="Star rating" />
+      <video controls>
+        <source src={clip.source_clip} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default Clip;
